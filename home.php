0031e66d02fea40dd1fcfccf093a03bc3687ca80
Lazar Narcis <lnarcis310@gmail.com>
2021-12-05 20:35:00 +0200

    Add script at the bottom of the page

--- home.php
+++ home.php
@@ -16,133 +16,6 @@
     <link rel="shortcut icon" href="logos/logo.png" type="image/x-icon">
     <link rel="stylesheet" href="style.css">
     <script src="jquery.js"></script>
-    <script>
-      $('textarea').keyup(function (event) {
-          if (event.keyCode == 13) {
-              var content = this.value;  
-              var caret = getCaret(this);          
-              if (event.shiftKey) {
-                this.value = content.substring(0, caret - 1) + "\n" + content.substring(caret, content.length);
-                event.stopPropagation();
-              } else {
-                this.value = content.substring(0, caret - 1) + content.substring(caret, content.length);
-                $('form').submit();
-              }
-          }
-      });
-      function showOptionsForMessage(val) {
-        let msj = document.getElementById("showTimes" + val);
-        let other = document.getElementById("idOfM" + val);
-        
-        if (msj.style.display === "none") {
-          msj.style = "display: inline;";
-          other.style = "background-color: #565171;";
-        } else {
-          msj.style = "display: none;";
-          other.style = "background-color: ##3a365b;";
-        }
-      }
-      var start = 0;
-      var path = location.href.substring(0, location.href.lastIndexOf("/")+1);
-      var url = path + '/chat.php';
-      $(document).ready(function(){
-        load();
-        $("form").submit(function(e){
-          $.post(url, {
-            message: $("#message").val()
-          });
-          $("#message").val('')
-          return false;
-        });
-      });
-      function load() {
-        $.get(url + '?start=' + start, function(result) {
-          if(result.items) {
-            result.items.forEach(item => {
-              start = item.id;
-              $("#messages").append(renderMessage(item));
-            });
-            $("#messages").animate({scrollTop: $("#messages")[0].scrollHeight}, 0);
-          }
-          load();
-        });
-      }
-      function renderMessage(item) {
-        let admin;
-        if (item.founder == 1) {
-          admin = " (F)";
-        } else if (item.admin == 1) {
-          admin = " (A)";
-        } else {
-          admin = "";
-        }
-        let x;
-        <?php 
-          $sql = "SELECT id, logged FROM users ORDER BY id=1";
-          $result = mysqli_query($link,$sql);
-          $row = mysqli_fetch_array($result, MYSQLI_NUM);
-          mysqli_free_result($result);
-        ?>
-        if(item.action == 1) {
-          x = `<div id="all-message">
-              <div class="actiontext">
-                <span>${item.actiontext} ${item.created_at}</span>
-              </div>
-            </div>`;
-        } else if (<?php echo $_SESSION['id']; ?> == item.userid) {
-          x = `<div id="all-message">
-                <div class="date">
-                  <div id="nameUser">
-                    <a id="user-profile-link" href="profile.php?id=${item.userid}">${item.name}</a>
-                    <span id="admin-text">${admin}</span>
-                  </div>
-                  <div><span class="time" id="showTimes${item.id}"><small>${item.created_at}</small></span></div>
-                </div>
-                <div class="messageO">
-                  <div>
-                    <img id="profile-message-picture" src="images/${item.file}" />
-                    `;
-                    if (<?php echo $row[1] ; ?>== 1) {
-                      x += `<span class="active-user" style="background-color: #0fbf15;"></span>`;
-                    } else {
-                      x += `<span class="active-user" style="background-color: grey;"></span>`;
-                    }
-                    x += `
-                  </div>
-                  <div class="msj" id="idOfM${item.id}" onclick="showOptionsForMessage(${item.id})">
-                    <span>${item.message}</span>
-                  </div>
-                </div>
-              </div>`;
-        } else {
-          x = `<div id="all-message">
-                <div class="date">
-                  <div id="nameUser">
-                    <a id="user-profile-link" href="profile.php?id=${item.userid}">${item.name}</a>
-                    <span id="admin-text">${admin}</span>
-                  </div>
-                  <div><span class="time" id="showTimes${item.id}"><small>${item.created_at}</small></span></div>
-                </div>
-                <div class="messageO">
-                  <div>
-                    <img id="profile-message-picture" src="images/${item.file}" />
-                    `;
-                    if (1== 1) {
-                      x += `<span class="active-user" style="background-color: #0fbf15;"></span>`;
-                    } else {
-                      x += `<span class="active-user" style="background-color: grey;"></span>`;
-                    }
-                    x += `
-                  </div>
-                  <div class="msj" id="idOfM${item.id}" style="background-color: #402e9f;" onclick="showOptionsForMessage(${item.id})">
-                    <span>${item.message}</span>
-                  </div>
-                </div>
-              </div>`;
-        }
-        return x;
-      }
-    </script>
     <style type="text/css">
       body {
         margin: 0;
@@ -298,5 +171,132 @@
       <input type="image" name="submit" src="logos/send.svg" alt="Submit" />
     </div>
     </form>
+    <script>
+      $('textarea').keyup(function (event) {
+          if (event.keyCode == 13) {
+              var content = this.value;  
+              var caret = getCaret(this);          
+              if (event.shiftKey) {
+                this.value = content.substring(0, caret - 1) + "\n" + content.substring(caret, content.length);
+                event.stopPropagation();
+              } else {
+                this.value = content.substring(0, caret - 1) + content.substring(caret, content.length);
+                $('form').submit();
+              }
+          }
+      });
+      function showOptionsForMessage(val) {
+        let msj = document.getElementById("showTimes" + val);
+        let other = document.getElementById("idOfM" + val);
+        
+        if (msj.style.display === "none") {
+          msj.style = "display: inline;";
+          other.style = "background-color: #565171;";
+        } else {
+          msj.style = "display: none;";
+          other.style = "background-color: ##3a365b;";
+        }
+      }
+      var start = 0;
+      var path = location.href.substring(0, location.href.lastIndexOf("/")+1);
+      var url = path + '/chat.php';
+      $(document).ready(function(){
+        load();
+        $("form").submit(function(e){
+          $.post(url, {
+            message: $("#message").val()
+          });
+          $("#message").val('')
+          return false;
+        });
+      });
+      function load() {
+        $.get(url + '?start=' + start, function(result) {
+          if(result.items) {
+            result.items.forEach(item => {
+              start = item.id;
+              $("#messages").append(renderMessage(item));
+            });
+            $("#messages").animate({scrollTop: $("#messages")[0].scrollHeight}, 0);
+          }
+          load();
+        });
+      }
+      function renderMessage(item) {
+        let admin;
+        if (item.founder == 1) {
+          admin = " (F)";
+        } else if (item.admin == 1) {
+          admin = " (A)";
+        } else {
+          admin = "";
+        }
+        let x;
+        <?php 
+          $sql = "SELECT id, logged FROM users ORDER BY id=1";
+          $result = mysqli_query($link,$sql);
+          $row = mysqli_fetch_array($result, MYSQLI_NUM);
+          mysqli_free_result($result);
+        ?>
+        if(item.action == 1) {
+          x = `<div id="all-message">
+              <div class="actiontext">
+                <span>${item.actiontext} ${item.created_at}</span>
+              </div>
+            </div>`;
+        } else if (<?php echo $_SESSION['id']; ?> == item.userid) {
+          x = `<div id="all-message">
+                <div class="date">
+                  <div id="nameUser">
+                    <a id="user-profile-link" href="profile.php?id=${item.userid}">${item.name}</a>
+                    <span id="admin-text">${admin}</span>
+                  </div>
+                  <div><span class="time" id="showTimes${item.id}"><small>${item.created_at}</small></span></div>
+                </div>
+                <div class="messageO">
+                  <div>
+                    <img id="profile-message-picture" src="images/${item.file}" />
+                    `;
+                    if (<?php echo $row[1] ; ?>== 1) {
+                      x += `<span class="active-user" style="background-color: #0fbf15;"></span>`;
+                    } else {
+                      x += `<span class="active-user" style="background-color: grey;"></span>`;
+                    }
+                    x += `
+                  </div>
+                  <div class="msj" id="idOfM${item.id}" onclick="showOptionsForMessage(${item.id})">
+                    <span>${item.message}</span>
+                  </div>
+                </div>
+              </div>`;
+        } else {
+          x = `<div id="all-message">
+                <div class="date">
+                  <div id="nameUser">
+                    <a id="user-profile-link" href="profile.php?id=${item.userid}">${item.name}</a>
+                    <span id="admin-text">${admin}</span>
+                  </div>
+                  <div><span class="time" id="showTimes${item.id}"><small>${item.created_at}</small></span></div>
+                </div>
+                <div class="messageO">
+                  <div>
+                    <img id="profile-message-picture" src="images/${item.file}" />
+                    `;
+                    if (1== 1) {
+                      x += `<span class="active-user" style="background-color: #0fbf15;"></span>`;
+                    } else {
+                      x += `<span class="active-user" style="background-color: grey;"></span>`;
+                    }
+                    x += `
+                  </div>
+                  <div class="msj" id="idOfM${item.id}" style="background-color: #402e9f;" onclick="showOptionsForMessage(${item.id})">
+                    <span>${item.message}</span>
+                  </div>
+                </div>
+              </div>`;
+        }
+        return x;
+      }
+    </script>
 </body>
 </html>
