842c0c491c5a3dcb2f05970a5331d20e8cf67be5
Lazar Narcis <lnarcis310@gmail.com>
2021-12-25 14:45:34 +0200

    Code better in showTicket.php

--- showTicket.php
+++ showTicket.php
@@ -2,8 +2,8 @@
   session_start();
   require "config.php";
   if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
-      header("location: login.php");
-      exit;
+    header("location: login.php");
+    exit;
   } else if(!isset($_GET['id'])) {
     header('Location: index.php');
     exit();
@@ -32,109 +32,110 @@
     <?php include_once("header.php"); ?>
     <div style="margin: 20px;">
       <div class="main-div">
-      <?php while ($query->fetch()):
-        if ($_SESSION['admin'] == 0 && $_SESSION['id'] != $userid) {
-          echo '<span class="user-error">You don\'t have access!</span>';
-          return;
-        } else {
-          ?>
-        <div class="secondary-div">
-          <?php
+      <?php 
+        while ($query->fetch()):
+          if ($_SESSION['admin'] == 0 && $_SESSION['id'] != $userid) {
+            echo '<span class="user-error">You don\'t have access!</span>';
+            return;
+          } else {
+            echo "<div class='secondary-div'>";
             if ($closed == 0) {
-              ?>
-                <div id="opened">
+              echo "
+                <div id='opened'>
                   <span>Opened</span>
                 </div>
-              <?php
+              ";
             } else {
-              ?>
-                <div id="opened" style="background-color: #611b0f;">
+              echo "
+                <div id='opened' style='background-color: #611b0f;'>
                   <span>Closed</span>
                 </div>
-              <?php
+              ";
             }
-          ?>
-          <h1>Ticket #<?php echo $ticketid ?></h1>
-          <span style="color:lightgrey">Subject: <?php echo $subject ?></span><br>
-          <span style="color:lightgrey">Message: <?php echo $texts ?></span><br>
-          <span style="color:lightgrey">Email: <?php echo $email ?></span><br>
-          <span style="color:lightgrey">Username: <a href="profile.php?id=<?php echo $userid ?>"><?php echo $username ?></a></span><br>
-          <span style="color:lightgrey">User ID: <?php echo $userid; ?></span><br>
-          <form action="sendMessageFromTicket.php" method="post" id="form">
-            <textarea type="text" name="message" class="user-input" placeholder="Reply as <?php echo $_SESSION["username"]; ?>..." autofocus <?php
-              if ($closed == 1) {
-                echo "disabled";
-              } else {
-                echo "";
-              }
-            ?>></textarea>
-            <input type="text" name="text" id="text" value="<?php echo $ticketid; ?>" style="display: none"><br/>
-            <input type="submit" class="user-button" value="Reply" <?php
-              if ($closed == 1) {
-                echo "disabled";
-              } else {
-                echo "";
-              }
-            ?>>
-          </form>
+      ?>
+      <h1>Ticket #<?php echo $ticketid ?></h1>
+      <span style="color:lightgrey">Subject: <?php echo $subject ?></span><br>
+      <span style="color:lightgrey">Message: <?php echo $texts ?></span><br>
+      <span style="color:lightgrey">Email: <?php echo $email ?></span><br>
+      <span style="color:lightgrey">Username: <a href="profile.php?id=<?php echo $userid ?>"><?php echo $username ?></a></span><br>
+      <span style="color:lightgrey">User ID: <?php echo $userid; ?></span><br>
+      <form action="sendMessageFromTicket.php" method="post" id="form">
+        <textarea type="text" name="message" class="user-input" placeholder="Reply as <?php echo $_SESSION["username"]; ?>..." autofocus 
           <?php
-            if ($_SESSION['admin'] == 1) {
-              if ($closed == 0) {
-                ?>
-                  <p><a href="closeTicket.php?id=<?php echo $ticketid; ?>">Close Ticket</a></p>
-                <?php
-              } else {
-                ?>
-                  <p><a href="openTicket.php?id=<?php echo $ticketid; ?>">Open Ticket</a> (You can't add comments until someone opens the ticket!)</p>
-                <?php
-              }
+            if ($closed == 1) {
+              echo "disabled";
             } else {
-              if ($closed == 1) {
-                ?>
-                  <p>You can't add comments until someone opens the ticket!</p>
-                <?php
-              }
+              echo "";
             }
           ?>
-          <h3>Comments:</h3>
-          <div style="display: flex; flex-direction: column-reverse;">
-            <?php
+        >
+        </textarea>
+        <input type="text" name="text" id="text" value="<?php echo $ticketid; ?>" style="display: none"><br/>
+        <input type="submit" class="user-button" value="Reply"
+          <?php
+            if ($closed == 1) {
+              echo "disabled";
+            } else {
+              echo "";
+            }
+          ?>
+        >
+      </form>
+      <?php
+        if ($_SESSION['admin'] == 1) {
+          if ($closed == 0) {
+            echo "<p><a href='closeTicket.php?id=$ticketid'>Close Ticket</a></p>";
+          } else {
+            echo "<p><a href='openTicket.php?id=$ticketid'>Open Ticket</a> (You can't add comments until someone opens the ticket!)</p>";
+          }
+        } else {
+          if ($closed == 1) {
+            echo "<p>You can't add comments until someone opens the ticket!</p>";
+          }
+        }
+      ?>
+      <h3>Comments:</h3>
+      <div style="display: flex; flex-direction: column-reverse;">
+      <?php
         $sql = "SELECT * FROM `comments` WHERE forTicket=$id";
         $querys = mysqli_query($link,$sql);
         if (mysqli_num_rows($querys) > 0) {
           while ($row= mysqli_fetch_assoc($querys)) {
-          ?>
-            <div id="comment">
-                <div id="name">
-                  <img src="<?php echo $row['file']; ?>" alt="Profile Picture" srcset="" id="profilePicture">
-                  <span style="margin-top: 7px; margin-left: 10px; color: white;"><b><a href="profile.php?id=<?php echo $row['userid']; ?>"><?php echo $row['username']; ?></a></b>
-                <?php
-                  if ($row['admin'] == 1) {
-                    echo "*technical support*";
-                  }
-                ?>
+            $file = $row['file'];
+            $userid = $row['userid'];
+            $username = $row['username'];
+            $admin = $row['admin'];
+            $text = $row['text'];
+            $created_at = $row['created_at'];
+            echo "
+              <div id='comment'>
+                <div id='name'>
+                <img src='$file' alt='Profile Picture' id='profilePicture'>
+                <span style='margin-top: 7px; margin-left: 10px; color: white;'><b><a href='profile.php?id=$userid'>$username</a></b>
+            ";
+            if ($admin == 1) {
+              echo "*technical support*";
+            }
+            echo "
                 </span>
-                </div>
-                <div id="message">
-                  <span style="white-space: pre-wrap;"><?php echo $row['text']; ?></span>
-                </div>
-                <small style="float: right;"><?php echo $row['created_at']; ?></small>
               </div>
-          <?php
+              <div id='message'>
+                <span style='white-space: pre-wrap;'>$text</span>
+              </div>
+              <small style='float: right;'>$created_at</small>
+            </div>";
           }
         } else {
-      ?>
-      <div id="comment"><p>No Comments.</p></div>
-      <?php
+          echo "<div id='comment'><p>No Comments.</p></div>";
         } 
+      echo "
+      </div>
+      <p id='data-sended'>The ticket was created at: $created_at</p>
+      ";
+          }
+        endwhile;
       ?>
-          </div>
-      <p id="data-sended">The ticket was created at: <?php echo $created_at ?></p>
-      <?php
-      }
-      endwhile;
-      ?>
-        </div>
+      </div>
     </div>
   </body>
 </html>
