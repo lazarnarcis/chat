22f45ccd8db41b7d4179d6c751dfbbc65c77f83f
Lazar Narcis <lnarcis310@gmail.com>
2021-12-25 09:27:53 +0200

    We add a TAB from the margin in the home.php file to make it look better

--- home.php
+++ home.php
@@ -8,121 +8,121 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height">
-    <title>Home</title>
-    <link rel="shortcut icon" href="logos/logo.png" type="image/x-icon">
-    <script src="jquery/jquery.js"></script>
-    <link rel="stylesheet" href="css/home.css?v=<?php echo time(); ?>">
-</head>
-<body>
-  <?php include_once("header.php"); ?>
-  <div class="home-page">
-    <h1 id="general-chat">General Chat</h1>
-    <div id="messages"></div>
-    <form>
-      <div id="inputs">
-        <input type="text" name="message" id="message" placeholder="Type a message..." autocomplete="off" autofocus />
-        <input type="image" name="submit" src="logos/send.svg" alt="Submit" />
-      </div>
-    </form>
-  </div>
-  <script>
-    $('textarea').keyup(function (event) {
-      if (event.keyCode == 13) {
-        var content = this.value;  
-        var caret = getCaret(this);          
-        if (event.shiftKey) {
-          this.value = content.substring(0, caret - 1) + "\n" + content.substring(caret, content.length);
-          event.stopPropagation();
-        } else {
-          this.value = content.substring(0, caret - 1) + content.substring(caret, content.length);
-          $('form').submit();
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height">
+      <title>Home</title>
+      <link rel="shortcut icon" href="logos/logo.png" type="image/x-icon">
+      <script src="jquery/jquery.js"></script>
+      <link rel="stylesheet" href="css/home.css?v=<?php echo time(); ?>">
+  </head>
+  <body>
+    <?php include_once("header.php"); ?>
+    <div class="home-page">
+      <h1 id="general-chat">General Chat</h1>
+      <div id="messages"></div>
+      <form>
+        <div id="inputs">
+          <input type="text" name="message" id="message" placeholder="Type a message..." autocomplete="off" autofocus />
+          <input type="image" name="submit" src="logos/send.svg" alt="Submit" />
+        </div>
+      </form>
+    </div>
+    <script>
+      $('textarea').keyup(function (event) {
+        if (event.keyCode == 13) {
+          var content = this.value;  
+          var caret = getCaret(this);          
+          if (event.shiftKey) {
+            this.value = content.substring(0, caret - 1) + "\n" + content.substring(caret, content.length);
+            event.stopPropagation();
+          } else {
+            this.value = content.substring(0, caret - 1) + content.substring(caret, content.length);
+            $('form').submit();
+          }
         }
-      }
-    });
-    function showOptionsForMessage(val) {
-      let msj = document.getElementById("showTimes" + val);
-      msj.style = "display: inline;";
-    }
-    function unshowOptionsForMessage(val) {
-      let msj = document.getElementById("showTimes" + val);
-      msj.style = "display: none;";
-    }
-    var start = 0;
-    var path = location.href.substring(0, location.href.lastIndexOf("/")+1);
-    var url = path + '/chat.php';
-    $(document).ready(function(){
-      load();
-      $("form").submit(function(e){
-        $.post(url, {
-          message: $("#message").val()
-        });
-        $("#message").val('')
-        return false;
       });
-    });
-    function load() {
-      $.get(url + '?start=' + start, function(result) {
-        if(result.items) {
-          result.items.forEach(item => {
-            start = item.id;
-            $("#messages").append(renderMessage(item));
-          });
-          $("#messages").animate({scrollTop: $("#messages")[0].scrollHeight}, 0);
-        }
+      function showOptionsForMessage(val) {
+        let msj = document.getElementById("showTimes" + val);
+        msj.style = "display: inline;";
+      }
+      function unshowOptionsForMessage(val) {
+        let msj = document.getElementById("showTimes" + val);
+        msj.style = "display: none;";
+      }
+      var start = 0;
+      var path = location.href.substring(0, location.href.lastIndexOf("/")+1);
+      var url = path + '/chat.php';
+      $(document).ready(function(){
         load();
+        $("form").submit(function(e){
+          $.post(url, {
+            message: $("#message").val()
+          });
+          $("#message").val('')
+          return false;
+        });
       });
-    }
-    function renderMessage(item) {
-      let admin;
-      if (item.founder == 1) {
-        admin = " (Founder)";
-      } else if (item.admin == 1) {
-        admin = " (Admin)";
-      } else {
-        admin = "";
+      function load() {
+        $.get(url + '?start=' + start, function(result) {
+          if(result.items) {
+            result.items.forEach(item => {
+              start = item.id;
+              $("#messages").append(renderMessage(item));
+            });
+            $("#messages").animate({scrollTop: $("#messages")[0].scrollHeight}, 0);
+          }
+          load();
+        });
       }
-      let x;
-      if (item.action == 1) {
-        x = `
-          <div id="all-message">
-            <div class="actiontext">
-              <span>${item.actiontext} ${item.created_at}</span>
-            </div>
-          </div>
-        `;
-      } else {
-        x = `
-          <div id="all-message">
-            <div class="date">
-              <div id="nameUser">
-                <a id="user-profile-link" href="profile.php?id=${item.userid}">${item.name}</a>
-                <span id="admin-text">${admin}</span>
+      function renderMessage(item) {
+        let admin;
+        if (item.founder == 1) {
+          admin = " (Founder)";
+        } else if (item.admin == 1) {
+          admin = " (Admin)";
+        } else {
+          admin = "";
+        }
+        let x;
+        if (item.action == 1) {
+          x = `
+            <div id="all-message">
+              <div class="actiontext">
+                <span>${item.actiontext} ${item.created_at}</span>
               </div>
             </div>
-            <div class="user-message">
-              <div>
-                <img id="profile-message-picture" src="${item.file}" /> 
+          `;
+        } else {
+          x = `
+            <div id="all-message">
+              <div class="date">
+                <div id="nameUser">
+                  <a id="user-profile-link" href="profile.php?id=${item.userid}">${item.name}</a>
+                  <span id="admin-text">${admin}</span>
+                </div>
               </div>
-              <span class="active-user" style="background-color: #0fbf15;"></span>
-              <div 
-                class="msj" 
-                onmouseover="showOptionsForMessage(${item.id})" 
-                onmouseout="unshowOptionsForMessage(${item.id})"
-                style="background-color: ${ item.userid == <?php echo $_SESSION['id']; ?> ? "#536160" : "#5d7191"}"
-              >
-                <span>${item.message}</span>
+              <div class="user-message">
+                <div>
+                  <img id="profile-message-picture" src="${item.file}" /> 
+                </div>
+                <span class="active-user" style="background-color: #0fbf15;"></span>
+                <div 
+                  class="msj" 
+                  onmouseover="showOptionsForMessage(${item.id})" 
+                  onmouseout="unshowOptionsForMessage(${item.id})"
+                  style="background-color: ${ item.userid == <?php echo $_SESSION['id']; ?> ? "#536160" : "#5d7191"}"
+                >
+                  <span>${item.message}</span>
+                </div>
+                <div id="timeS"><span class="time" id="showTimes${item.id}" style="display: none;"><small>${item.created_at}</small></span></div>
               </div>
-              <div id="timeS"><span class="time" id="showTimes${item.id}" style="display: none;"><small>${item.created_at}</small></span></div>
             </div>
-          </div>
-        `;
+          `;
+        }
+        return x;
       }
-      return x;
-    }
-  </script>
-</body>
+    </script>
+  </body>
 </html>
