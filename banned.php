1f0120ccb9caa37d65105d4580f0a251a436a9d4
Lazar Narcis <lnarcis310@gmail.com>
2021-12-25 09:03:28 +0200

    Make banned.php looks better

--- banned.php
+++ banned.php
@@ -2,8 +2,8 @@
   session_start();
   require "config.php";
   if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
-      header("location: login.php");
-      exit;
+    header("location: login.php");
+    exit;
   }
 ?> 
 <!DOCTYPE html>
@@ -17,33 +17,34 @@
   <link rel="stylesheet" href="css/banned.css?v=<?php echo time(); ?>">
 </head>
 <body>
-	 <?php include_once("header.php"); ?>
-   <div style="margin: 20px;">
-    <h1>Banned Users</h1>
-        <?php 
-        $banned_users = 0;
-          if(isset($_SESSION['username'])) { 
-            $usern = $_SESSION['id']; 
-            $sql="SELECT * FROM `users` WHERE banned=1"; 
-            $query = mysqli_query($link,$sql);
-            if (mysqli_num_rows($query) > 0) {
-                while ($row= mysqli_fetch_assoc($query)) {
-        ?>
-        <div class="boxes">
-          <span style="color:black;"><img id="image" src="<?php echo $row['file']; ?>" alt="Profile"></span><br id="for-phone" />
-          <a id="link-profile" href="profile.php?id=<?php echo $row['id']; ?>"><?php echo $row['username']; ?></a>
-        </div>
-        <br>
-    <?php
-    $banned_users ++;
-      }
-    } else {
-?>
-<div><p>There are no banned users.</p></div>
-<?php
-  } 
-}
-echo "Total users banned: $banned_users";
-?>
+  <?php include_once("header.php"); ?>
+  <div style="margin: 20px;">
+  <h1>Banned Users</h1>
+  <?php 
+    $banned_users = 0;
+    if(isset($_SESSION['username'])) { 
+      $usern = $_SESSION['id']; 
+      $sql="SELECT * FROM `users` WHERE banned=1"; 
+      $query = mysqli_query($link, $sql);
+      if (mysqli_num_rows($query) > 0) {
+        while ($row = mysqli_fetch_assoc($query)) {
+          $file = $row['file'];
+          $id = $row['id'];
+          $username = $row['username'];
+          echo "
+            <div class='boxes'>
+              <span style='color:black;'><img id='image' src='$file' alt='Profile'></span><br id='for-phone' />
+              <a id='link-profile' href='profile.php?id=$id'>$username</a>
+            </div><br>
+          ";
+          $banned_users ++;
+        }
+      } else {
+        echo '<div><p>There are no banned users.</p></div>';
+      } 
+    }
+    echo "Total banned users: $banned_users";
+  ?>
+  </div>
 </body>
 </html>
