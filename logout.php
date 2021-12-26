1d11d89d62d50fa21e6e1ce39b3c404f2cf24fdd
Lazar Narcis <lnarcis310@gmail.com>
2021-12-26 10:23:11 +0200

    Delete unnecessary variables

--- logout.php
+++ logout.php
@@ -4,9 +4,9 @@
     $id = $_SESSION['id'];
     $username = $_SESSION['username'];
     $sql = "UPDATE users SET logged=0 WHERE id=".$id."";
-    $query = mysqli_query($link, $sql);
-    $sqlx = "INSERT INTO chat (action, actiontext) VALUES ('1', '$username just disconnected!')";
-    $queryx = mysqli_query($link, $sqlx);
+    mysqli_query($link, $sql);
+    $sql = "INSERT INTO chat (action, actiontext) VALUES ('1', '$username just disconnected!')";
+    mysqli_query($link, $sql);
     session_reset();
     session_destroy();
     header("location: home.php");
