664cce61da4920607248d3cd89766b9c4f863ee2
Lazar Narcis <lnarcis310@gmail.com>
2021-12-29 12:23:34 +0200

    add username on notifications page

--- notifications.php
+++ notifications.php
@@ -11,10 +11,10 @@
     $id = $_GET['id'];
   }
   $queryString = "SELECT id, created_at, userid, text FROM notifications WHERE userid='$id' ORDER BY id DESC"; 
-  $query = $link->prepare($queryString);
-  $query->execute();
-  $query->store_result();
-  $query->bind_result($notifid, $created_at, $userid, $texts);
+  $query2 = $link->prepare($queryString);
+  $query2->execute();
+  $query2->store_result();
+  $query2->bind_result($notifid, $created_at, $userid, $texts);
   if ($_SESSION['admin'] == 0 && $_SESSION['id'] != $id) {
     header("location: home.php");
     return;
@@ -33,9 +33,18 @@
   <body>
     <?php require_once("header.php"); ?>
       <div style="margin: 20px;">
-        <h1>Notifications</h1>
+        <?php
+          $sql = "SELECT username FROM users WHERE id=$id";
+          $query1 = mysqli_query($link, $sql);
+          if (mysqli_num_rows($query1) > 0) {
+            while ($row = mysqli_fetch_assoc($query1)) {
+              $user = $row['username'];
+              echo "<h1>$user's Notifications</h1>";
+            }
+          }
+        ?>
         <div class="main-div">
-          <?php while ($query->fetch()):
+          <?php while ($query2->fetch()):
             echo "
               <div class='secondary-div'>
                 <span>$texts</span>
