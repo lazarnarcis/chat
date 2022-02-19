5fa0cbf171ad7dc2027ecdb0ccdc7cdfd5896797
Lazar Narcis <lnarcis310@gmail.com>
2022-02-19 11:29:23 +0200

    code better in login.php

--- login.php
+++ login.php
@@ -7,71 +7,76 @@
     require "config/config.php";
     $username = $password = "";
     $username_err = $password_err = "";
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $set_username = htmlspecialchars($_POST["username"]);
-        $set_password = htmlspecialchars($_POST["password"]);
 
-        if (empty($set_username)) {
-            $username_err = "Please enter username/email.";
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+            $serverip = $_SERVER['HTTP_CLIENT_IP'];
+        } else if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $serverip = $_SERVER['HTTP_X_FORWARDED_FOR'];
         } else {
-            $username = $set_username;
+            $serverip = $_SERVER['REMOTE_ADDR'];
         }
-        if (empty($set_password)) {
+
+        $username = htmlspecialchars($_POST['username']);
+        $password = htmlspecialchars($_POST['password']);
+        
+        if (empty($username)) {
+            $username_err = "Please enter username/email.";
+        } else if (empty($password)) {
             $password_err = "Please enter your password.";
         } else {
-            $password = $set_password;
-        }
-        if (empty($username_err) && empty($password_err)) {
-            if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-                $serverip = $_SERVER['HTTP_CLIENT_IP'];
-            } else if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-                $serverip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+            $sql = "SELECT * FROM users WHERE username='$username' OR email='$username'";
+            $result = mysqli_query($link, $sql);
+            if (mysqli_num_rows($result) == 0) {
+                $username_err = "No account found with that username/email.";
             } else {
-                $serverip = $_SERVER['REMOTE_ADDR'];
-            }
-            $sql = "SELECT id, username, password, admin, created_at, email, bio, file, founder, banned, logged, ip, last_ip, verified FROM users WHERE username = ? OR email = ?";
-            if ($stmt = mysqli_prepare($link, $sql)) {
-                mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_username);
-                $param_username = $username;
-                if (mysqli_stmt_execute($stmt)) {
-                    mysqli_stmt_store_result($stmt);
-                    if (mysqli_stmt_num_rows($stmt) == 1) {                
-                        mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password, $admin, $created_at, $email, $bio, $file, $founder, $banned, $logged, $ip, $last_ip, $verified);
-                        if (mysqli_stmt_fetch($stmt)) {
-                            if (password_verify($password, $hashed_password)) {
-                                $_SESSION["loggedin"] = true;
-                                $_SESSION["id"] = $id;
-                                $_SESSION["username"] = $username;  
-                                $_SESSION["admin"] = $admin;  
-                                $_SESSION["created_at"] = $created_at;
-                                $_SESSION["bio"] = $bio;  
-                                $_SESSION["file"] = $file;  
-                                $_SESSION["email"] = $email;  
-                                $_SESSION["founder"] = $founder;  
-                                $_SESSION["banned"] = $banned;  
-                                $_SESSION["logged"] = $logged;
-                                $_SESSION["ip"] = $ip;
-                                $_SESSION["last_ip"] = $last_ip;
-                                $_SESSION["verified"] = $verified;
-                                $sql = "UPDATE users SET last_ip='".$serverip."', logged=1 WHERE id='".$_SESSION["id"]."'";
-                                mysqli_query($link, $sql);
-                                $sql = "INSERT INTO chat (action, actiontext) VALUES ('1', '$username just connected!')";
-                                mysqli_query($link, $sql);
-                                header("location: home.php");
-                            } else {
-                                $password_err = "The password you entered was not valid.";
-                            }
-                        }
-                    } else {
-                        $username_err = "No account found with that username/email.";
-                    }
-                } else {
-                    $username_err = "Oops! Something went wrong. Please try again later.";
+                $row = mysqli_fetch_assoc($result);
+                $hashed_password = $row['password'];
+
+                if (!password_verify($password, $hashed_password)) {
+                    $password_err = "The password you entered was not valid.";
                 }
-                mysqli_stmt_close($stmt);
             }
         }
-        mysqli_close($link);
+
+        if (empty($username_err) && empty($password_err)) {
+            $sql = "SELECT * FROM users WHERE username='$username'";
+            $result = mysqli_query($link, $sql);
+            $row = mysqli_fetch_assoc($result);
+
+            $id = $row['id'];
+            $admin = $row['admin'];
+            $created_at = $row['created_at'];
+            $bio = $row['bio'];
+            $file = $row['file'];
+            $email = $row['email'];
+            $founder = $row['founder'];
+            $banned = $row['banned'];
+            $logged = $row['logged'];
+            $ip = $row['ip'];
+            $last_ip = $row['last_ip'];
+            $verified = $row['verified'];
+
+            $_SESSION["loggedin"] = true;
+            $_SESSION["id"] = $id;
+            $_SESSION["username"] = $username;  
+            $_SESSION["admin"] = $admin;  
+            $_SESSION["created_at"] = $created_at;
+            $_SESSION["bio"] = $bio;  
+            $_SESSION["file"] = $file;  
+            $_SESSION["email"] = $email;  
+            $_SESSION["founder"] = $founder;  
+            $_SESSION["banned"] = $banned;  
+            $_SESSION["logged"] = $logged;
+            $_SESSION["ip"] = $ip;
+            $_SESSION["last_ip"] = $last_ip;
+            $_SESSION["verified"] = $verified;
+            $sql = "UPDATE users SET last_ip='".$serverip."', logged=1 WHERE id='".$_SESSION["id"]."'";
+            mysqli_query($link, $sql);
+            $sql = "INSERT INTO chat (action, actiontext) VALUES ('1', '$username just connected!')";
+            mysqli_query($link, $sql);
+            header("location: home.php");
+        }
     }
 ?>
 <!DOCTYPE html>
