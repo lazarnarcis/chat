6a1d49c1c696c947484294893c9276f75db1df2d
Lazar Narcis <lnarcis310@gmail.com>
2022-02-19 11:53:27 +0200

    add login to actions.php

--- actions.php
+++ actions.php
@@ -6,8 +6,7 @@
 
     if (empty($action)) {
         header("location: index.php");
-    }
-    if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
+    } else if ((!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !=true) && $_GET['action'] != "login") {
         header("location: login.php");
         exit;
     }
@@ -15,12 +14,88 @@
     require 'PHPMailer-master/src/Exception.php';
     require 'PHPMailer-master/src/PHPMailer.php';
     require 'PHPMailer-master/src/SMTP.php';
+    require "gmail_account/gmail_account.php";
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;
 
-    require "gmail_account/gmail_account.php";
+    if ($action == "login") {
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+            $serverip = $_SERVER['HTTP_CLIENT_IP'];
+        } else if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $serverip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        } else {
+            $serverip = $_SERVER['REMOTE_ADDR'];
+        }
+        $acces = 1;
+        $username = htmlspecialchars($_POST['username']);
+        $password = htmlspecialchars($_POST['password']);
+        
+        if (empty($username)) {
+            $username_err = "Please enter username/email.";
+            header("location: login.php?username_err=".$username_err."");
+            $acces = 0;
+        } else if (empty($password)) {
+            $password_err = "Please enter your password.";
+            header("location: login.php?password_err=".$password_err."");
+            $acces = 0;
+        } else {
+            $sql = "SELECT * FROM users WHERE username='$username' OR email='$username'";
+            $result = mysqli_query($link, $sql);
+            if (mysqli_num_rows($result) == 0) {
+                $username_err = "No account found with that username/email.";
+                header("location: login.php?username_err=".$username_err."");
+                $acces = 0;
+            } else {
+                $row = mysqli_fetch_assoc($result);
+                $hashed_password = $row['password'];
 
-    if ($action == "ban") {
+                if (!password_verify($password, $hashed_password)) {
+                    $password_err = "The password you entered was not valid.";
+                    header("location: login.php?password_err=".$password_err."");
+                    $acces = 0;
+                }
+            }
+        }
+
+        if ($acces == 1) {
+            $sql = "SELECT * FROM users WHERE username='$username'";
+            $result = mysqli_query($link, $sql);
+            $row = mysqli_fetch_assoc($result);
+
+            $id = $row['id'];
+            $admin = $row['admin'];
+            $created_at = $row['created_at'];
+            $bio = $row['bio'];
+            $file = $row['file'];
+            $email = $row['email'];
+            $founder = $row['founder'];
+            $banned = $row['banned'];
+            $logged = $row['logged'];
+            $ip = $row['ip'];
+            $last_ip = $row['last_ip'];
+            $verified = $row['verified'];
+
+            $_SESSION["loggedin"] = true;
+            $_SESSION["id"] = $id;
+            $_SESSION["username"] = $username;  
+            $_SESSION["admin"] = $admin;  
+            $_SESSION["created_at"] = $created_at;
+            $_SESSION["bio"] = $bio;  
+            $_SESSION["file"] = $file;  
+            $_SESSION["email"] = $email;  
+            $_SESSION["founder"] = $founder;  
+            $_SESSION["banned"] = $banned;  
+            $_SESSION["logged"] = $logged;
+            $_SESSION["ip"] = $ip;
+            $_SESSION["last_ip"] = $last_ip;
+            $_SESSION["verified"] = $verified;
+            $sql = "UPDATE users SET last_ip='".$serverip."', logged=1 WHERE id='".$_SESSION["id"]."'";
+            mysqli_query($link, $sql);
+            $sql = "INSERT INTO chat (action, actiontext) VALUES ('1', '$username just connected!')";
+            mysqli_query($link, $sql);
+            header("location: home.php");
+        }
+    } else if ($action == "ban") {
         if (!isset($_GET['id'])) {
             header('Location: index.php');
             exit();
--- login.php
+++ login.php
@@ -4,79 +4,12 @@
         header("location: home.php");
         exit;
     }
-    require "config/config.php";
-    $username = $password = "";
     $username_err = $password_err = "";
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-            $serverip = $_SERVER['HTTP_CLIENT_IP'];
-        } else if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-            $serverip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-        } else {
-            $serverip = $_SERVER['REMOTE_ADDR'];
-        }
-
-        $username = htmlspecialchars($_POST['username']);
-        $password = htmlspecialchars($_POST['password']);
-        
-        if (empty($username)) {
-            $username_err = "Please enter username/email.";
-        } else if (empty($password)) {
-            $password_err = "Please enter your password.";
-        } else {
-            $sql = "SELECT * FROM users WHERE username='$username' OR email='$username'";
-            $result = mysqli_query($link, $sql);
-            if (mysqli_num_rows($result) == 0) {
-                $username_err = "No account found with that username/email.";
-            } else {
-                $row = mysqli_fetch_assoc($result);
-                $hashed_password = $row['password'];
-
-                if (!password_verify($password, $hashed_password)) {
-                    $password_err = "The password you entered was not valid.";
-                }
-            }
-        }
-
-        if (empty($username_err) && empty($password_err)) {
-            $sql = "SELECT * FROM users WHERE username='$username'";
-            $result = mysqli_query($link, $sql);
-            $row = mysqli_fetch_assoc($result);
-
-            $id = $row['id'];
-            $admin = $row['admin'];
-            $created_at = $row['created_at'];
-            $bio = $row['bio'];
-            $file = $row['file'];
-            $email = $row['email'];
-            $founder = $row['founder'];
-            $banned = $row['banned'];
-            $logged = $row['logged'];
-            $ip = $row['ip'];
-            $last_ip = $row['last_ip'];
-            $verified = $row['verified'];
-
-            $_SESSION["loggedin"] = true;
-            $_SESSION["id"] = $id;
-            $_SESSION["username"] = $username;  
-            $_SESSION["admin"] = $admin;  
-            $_SESSION["created_at"] = $created_at;
-            $_SESSION["bio"] = $bio;  
-            $_SESSION["file"] = $file;  
-            $_SESSION["email"] = $email;  
-            $_SESSION["founder"] = $founder;  
-            $_SESSION["banned"] = $banned;  
-            $_SESSION["logged"] = $logged;
-            $_SESSION["ip"] = $ip;
-            $_SESSION["last_ip"] = $last_ip;
-            $_SESSION["verified"] = $verified;
-            $sql = "UPDATE users SET last_ip='".$serverip."', logged=1 WHERE id='".$_SESSION["id"]."'";
-            mysqli_query($link, $sql);
-            $sql = "INSERT INTO chat (action, actiontext) VALUES ('1', '$username just connected!')";
-            mysqli_query($link, $sql);
-            header("location: home.php");
-        }
+    if (!empty($_GET['username_err'])) {
+        $username_err = $_GET['username_err'];
+    }
+    if (!empty($_GET['password_err'])) {
+        $password_err = $_GET['password_err'];
     }
 ?>
 <!DOCTYPE html>
@@ -91,11 +24,11 @@
     </head>
     <body>
         <div class="wrapper">
-            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <form action="actions.php?action=login" method="post">
                 <div id="menu">
                     <h1>Login</h1>
                     <div>
-                        <input type="text" name="username" class="user-input" value="<?php echo $username; ?>" placeholder="Username or Email">
+                        <input type="text" name="username" class="user-input" placeholder="Username or Email">
                         <br>
                         <span class="user-error"><?php echo $username_err; ?></span>
                     </div>    
