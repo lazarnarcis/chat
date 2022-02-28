10fe0f94dd969d08862e1d3bc8e914b7868a7ada
Lazar Narcis <lnarcis310@gmail.com>
2022-02-28 21:27:27 +0200

    we add the page only for the founders (https://github.com/lazarnarcis/chat/issues/21)

--- logs.php
+++ logs.php
@@ -2,7 +2,7 @@
     session_start();
     require "config/config.php";
 
-    if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
+    if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true || $_SESSION['founder'] == 0) {
         header("location: login.php");
         exit;
     }
