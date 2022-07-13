473f9679e9678608b41b8b1fc5ee7a593bf9bb83
Lazar Narcis <lnarcis310@gmail.com>
2022-07-13 22:32:36 +0300

    bug fixes

--- actions.php
+++ actions.php
@@ -626,36 +626,6 @@
             $result = mysqli_query($link, $sql);
             header("location: showTicket.php?id=$text");
         }
-    } else if ($action == "confirm_account") {
-        if (!isset($_GET['id'])) {
-            header('Location: index.php');
-            exit();
-        } else {
-            $id = $_GET['id'];
-        
-            if ($_SESSION['id'] != $id) {
-                header("location: home.php");
-                return;
-            }
-        
-            $queryString = "SELECT * FROM users WHERE id='$id' ORDER BY id DESC LIMIT 1"; 
-            $result = mysqli_query($link, $queryString);
-            $row = mysqli_fetch_assoc($result);
-        
-            $user_id = $row['id'];
-            $username = $row['username'];
-        
-            $sql = "UPDATE users SET verified=1 WHERE id='$user_id'";
-            mysqli_query($link, $sql);
-            $sql = "INSERT INTO notifications (text, userid) VALUES ('Your account has been verified.', '".$user_id."')";
-            mysqli_query($link, $sql);
-            $lastname = $_SESSION['username'];
-            $sql = "INSERT INTO chat (action, actiontext) VALUES ('1', '$username just verified his account!')";
-            mysqli_query($link, $sql);
-
-            $err_message = "Your account has been confirmed!";
-            header('location: profile.php?id='.$user_id.'&err_message='.$err_message.'');
-        }
     } else if ($action == "create_ticket") {
         $set_message = htmlspecialchars($_POST['message']);
         $acces = 1;
@@ -1084,7 +1054,7 @@
             $account_name = $_SESSION['username'];
             $account_id = $_SESSION['id'];
             $account_email = $_SESSION['email'];
-            $actual_link = "https://$_SERVER[SERVER_NAME]/login.php?redirect_link=actions.php?action=confirm_account&id=$account_id";
+            $actual_link = "https://$_SERVER[SERVER_NAME]/login.php?redirect_link=confirm-account.php?id=$account_id";
 
             $localhost = array(
                 '127.0.0.1',
---
+++ confirm-account.php
@@ -0,0 +1,36 @@
+<?php
+    session_start();
+    require 'config/config.php';
+
+    if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
+        header("location: login.php");
+        exit;
+    } else if (!isset($_GET['id'])) {
+        header('location: index.php');
+        exit();
+    } else {
+        $id = $_GET['id'];
+        if ($_SESSION['id'] != $id) {
+            $err_message = "You can only confirm the account you are logged in to!";
+            header("location: home.php&err_message=$err_message");
+            return;
+        }
+        $queryString = "SELECT * FROM users WHERE id='$id' ORDER BY id DESC LIMIT 1"; 
+        $result = mysqli_query($link, $queryString);
+        $row = mysqli_fetch_assoc($result);
+
+        $user_id = $row['id'];
+        $username = $row['username'];
+
+        $sql = "UPDATE users SET verified=1 WHERE id='$user_id'";
+        mysqli_query($link, $sql);
+        $sql = "INSERT INTO notifications (text, userid) VALUES ('Your account has been verified.', '".$user_id."')";
+        mysqli_query($link, $sql);
+        $lastname = $_SESSION['username'];
+        $sql = "INSERT INTO chat (action, actiontext) VALUES ('1', '$username just verified his account!')";
+        mysqli_query($link, $sql);
+
+        $err_message = "Your account has been confirmed!";
+        header('location: profile.php?id='.$user_id.'&err_message='.$err_message.'');
+    }
+?>
